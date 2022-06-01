fbd77480eb5c17daf01ad02d2e2c6bcc66c4c03f
Joseph Prince <stonewahl@gmail.com>
2022-05-31 22:22:02 -0600

    add plot

--- stockPanel.py
+++ stockPanel.py
@@ -4,4 +4,8 @@ import yfinance as yf
 
 st.write("""# Stock Tracker""")
 
+symbol = 'xau'
+data = yf.Ticker(symbol)
+df = data.history(period='1d',start='2010-01-01',end='2022-05-1')
+st.line_chart(df.Close)
 
