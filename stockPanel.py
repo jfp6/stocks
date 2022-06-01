a44ed0938153e71c9fccc5a43e2b28ac56f4532e
Joseph Prince <stonewahl@gmail.com>
2022-05-31 22:29:14 -0600

    add tsla

--- stockPanel.py
+++ stockPanel.py
@@ -2,11 +2,16 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
-st.write("""# Stock Tracker
-### Dow Jones """)
+st.write("""# Stock Tracker""")
+st.write("""### Dow Jones """)
 
 symbol = 'dji'
 data = yf.Ticker(symbol)
 df = data.history(period='1d',start='2010-01-01',end='2022-05-1')
 st.line_chart(df.Close)
 
+st.write("""### TESLA """)
+symbol = 'tsla'
+data = yf.Ticker(symbol)
+df = data.history(period='1d',start='2010-01-01',end='2022-05-1')
+st.line_chart(df.Close)
