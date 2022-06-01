b013ed5faa3e9dadab9668ee46aa6ce15f92839e
Joseph Prince <stonewahl@gmail.com>
2022-05-31 22:24:26 -0600

    dji

--- stockPanel.py
+++ stockPanel.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
-st.write("""# Stock Tracker""")
+st.write("""# Stock Tracker
+### Dow Jones """)
 
-symbol = 'xau'
+symbol = 'dji'
 data = yf.Ticker(symbol)
 df = data.history(period='1d',start='2010-01-01',end='2022-05-1')
 st.line_chart(df.Close)
